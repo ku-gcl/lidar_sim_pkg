770849d3fd963ad4cb659cd5a60141e604783efe
SHIO777 <shiotatsu1015@gmail.com>
2025-08-19 14:54:11 +0900

    update: merge bridge to simulation

--- launch/simulation.launch.py
+++ launch/simulation.launch.py
@@ -30,5 +30,33 @@ def generate_launch_description():
         name="lidar_node",
         output="screen",
     )
-
-    return LaunchDescription([set_res_path, gz_sim, lidar_node])
+    
+    gz_topic = '/lidar/points'
+    ros_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        name='ros_gz_bridge',
+        arguments=[
+            # Gazebo -> ROS 2
+            f'{gz_topic}@sensor_msgs/msg/PointCloud2[gz.msgs.PointCloudPacked'
+        ],
+        remappings=[(gz_topic, "/livox/lidar")],
+        output='screen',
+    )
+    
+    # tf2_ros = Node(
+    #     package='tf2_ros',
+    #     executable='static_transform_publisher',
+    #     name='static_tf_laser',
+    #     # SDFの親リンク名は "link" なので、base_link ではなく link に合わせるのが安全
+    #     arguments=['0', '0', '0', '0', '0', '0', 'link', 'laser_link'],
+    #     output='screen',
+    # )
+    
+    return LaunchDescription([
+        set_res_path, 
+        gz_sim, 
+        lidar_node,
+        ros_bridge,
+        # tf2_ros,
+    ])
