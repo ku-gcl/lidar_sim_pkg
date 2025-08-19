962adc690e8a27b4bf82642e9041405ba03ce260
SHIO777 <shiotatsu1015@gmail.com>
2025-08-19 14:51:19 +0900

    refactor: bridge

--- launch/bridge.launch.py
+++ launch/bridge.launch.py
@@ -3,23 +3,28 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     gz_topic = '/lidar/points'
+    ros_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        name='ros_gz_bridge',
+        arguments=[
+            # Gazebo -> ROS 2
+            f'{gz_topic}@sensor_msgs/msg/PointCloud2[gz.msgs.PointCloudPacked'
+        ],
+        remappings=[(gz_topic, "/livox/lidar")],
+        output='screen',
+    )
+    
+    # tf2_ros = Node(
+    #     package='tf2_ros',
+    #     executable='static_transform_publisher',
+    #     name='static_tf_laser',
+    #     # SDFの親リンク名は "link" なので、base_link ではなく link に合わせるのが安全
+    #     arguments=['0', '0', '0', '0', '0', '0', 'link', 'laser_link'],
+    #     output='screen',
+    # )
+    
     return LaunchDescription([
-        Node(
-            package='ros_gz_bridge',
-            executable='parameter_bridge',
-            name='ros_gz_bridge',
-            arguments=[
-                # Gazebo -> ROS の一方向ブリッジ
-                f'{gz_topic}@sensor_msgs/msg/PointCloud2[gz.msgs.PointCloudPacked'
-            ],
-            output='screen',
-        ),
-        # Node(
-        #     package='tf2_ros',
-        #     executable='static_transform_publisher',
-        #     name='static_tf_laser',
-        #     # SDFの親リンク名は "link" なので、base_link ではなく link に合わせるのが安全
-        #     arguments=['0', '0', '0', '0', '0', '0', 'link', 'laser_link'],
-        #     output='screen',
-        # ),
+        ros_bridge,
+        # tf2_ros,
     ])
